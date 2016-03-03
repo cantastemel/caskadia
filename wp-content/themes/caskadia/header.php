a670b0c739cae9e1111c20b5d93fa391230b06ef
Jon Tastemel <jon.tastemel@goclio.com>
2016-03-02 19:27:06 -0800

    mobile wp nav

--- wp-content/themes/caskadia/header.php
+++ wp-content/themes/caskadia/header.php
@@ -25,12 +25,7 @@
         <button class="close-btn"><i class="fi-list"></i></button>
     <div class="mobile-nav-container">
         <nav class="mobile-nav">
-            <ul>
-                <li><a href="/" class="current">Home</a></li>
-                <li><a href="team.html">Team</a></li>
-                <li><a href="jobs.html">Jobs</a></li>
-                <li><a href="contact-us.html">Contact Us</a></li>
-            </ul>
+            <?php wp_nav_menu( array('menu' => 'Main nav' )); ?>
         </nav>
     </div>
     <div id="contentarea">
